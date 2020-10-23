42923dc5768574917e01ffdc98c2942f6f9b6f55
Robbie-Wadud <amaan.wadud@gmail.com>
2020-10-22 22:31:52 -0500

    Created and completed the header file for the exception handlling

---
+++ shared/Exception.h
@@ -0,0 +1,37 @@
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+#include <exception>
+#include <string>
+
+class Exception : public std::exception
+{
+public:
+        Exception(std::string e) {error=e; file = "unknown"; line = 0; fileline_set = false;}
+        Exception(std::string e, std::string f, int l) {error = e; file = f; line = l;; fileline_set = true;}
+
+        const char * what() const throw ()
+        {
+                std::string str;
+
+                if(fileline_set)
+                {
+                        str = "There was an exception thrown: [" + file + ": " + std::to_string(line) + "]: " + error;
+                }
+
+                else
+                {
+                        str = "There was an exception thrown: " + error;
+                }
+
+                constant char * c = str.c_str();
+                return c;
+        }
+
+        std::string error;
+        std::string file;
+        int line;
+        bool fileline_set;
+};
+
+#endif /* EXCEPTION_H */

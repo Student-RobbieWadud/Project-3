0c4b13650d725431ac7eea88de6e55c0408ad260
Robbie-Wadud <amaan.wadud@gmail.com>
2020-10-22 22:46:56 -0500

    Completed the shared memory header file

---
+++ shared/SharedMem.h
@@ -0,0 +1,26 @@
+#ifndef SHAREDMEM_H
+#define SHAREDMEM_H
+
+#include <sys/shm.h>
+#include "TimeType.h"
+
+class SharedMem
+{
+public:
+        SharedMem();
+        SharedMem(bool);
+        void setClock(int, int);
+        int getSeconds();
+        long getNanoSeconds();
+        TimeType getTime();
+        void increment();
+        void close();
+private:
+        void setupSharedMem();
+        bool isChildProc;
+        int segmentId;
+        int *seconds;
+        long *nanoSeconds;
+};
+
+#endif /* SHAREDMEM_H */
